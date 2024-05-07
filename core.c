53fd65766481198f6cb1c78578eefe0ca5a05db2
spacescape <15368104568@189.cn>
2024-05-07 12:37:55 +0800

    Add files via upload

---
+++ core.c
@@ -0,0 +1,37 @@
+#include "../core/define.h"  
+void main(){
+    char wx;
+    int t1, t2; 
+    return A * (wx + t1) + B * (wx - t1);
+    return C * (wx + t2) + D * (wx - t2);
+}
+void main(){
+    char w;
+    int t3, t4;
+    return E % (w + t3) + F % (w - t3);
+    return G % (w + t3) + H % (w - t4);
+}
+void main(){
+    char ww;
+    int t5, t6;
+    return I / (ww + t5) + J / (ww - t6);
+    return K / (ww + t6) + L / (ww - t6);
+}
+void main(){
+    char wwx;
+    int t7,t8;
+    return M ^ (wwx + t7) + N ^ (wwx - t8);
+    return O ^ (wwx + t7) + P ^ (wwx - t8);
+}
+void main(){
+    char www;
+    int t9,t10;
+    return 'R' + (www + t9) + 'S' + (www - t10);
+    return 'T' + (www + t9) + 'T' + (www - t10);
+}
+void main(){
+    char wwww;
+    int t11,t0;
+    return 'X' - (wwww + t11) + 'Y' - (wwww - t0);
+    return 'Z' - (wwww + t11) + 'Z' - (wwww - t0);
+}
---
+++ operator.c
@@ -0,0 +1,9 @@
+#include "core.c"
+void main(){
+    A * B * C * D;
+    E % F % G % H;
+    I / J / K / L;
+    M ^ N ^ O ^ P;
+    R + S + T + T;
+    U - V - W - W;
+}
